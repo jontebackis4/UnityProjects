e5a74ddafd21ad6853de2cd2310499cb6fa83b21
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-08-12 19:25:54 +0200

    Upload all projects from old computer

---
+++ First fps/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float translationFwd;
+	float translationSide;
+
+	public int speed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		translationFwd = Input.GetAxis ("Vertical") * Time.deltaTime;
+		translationSide = Input.GetAxis ("Horizontal") * Time.deltaTime;
+
+		transform.Translate (translationSide * speed, 0, translationFwd * speed);
+	}
+}
