c315f8219968bde184c04acc3b564ef9bce81ece
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-11-30 18:50:40 +0100

    Init Test Top Down

---
+++ Test games/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float mousePosition = Ca
+	}
+}
---
+++ Top down/Assets/PlayerMobility.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMobility : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody2D rb;
+
+	void start(){
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
+
+		transform.rotation = rot;
+		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
+		rb.angularVelocity = 0;
+
+
+		float input = Input.GetAxis ("Vertical");
+		rb.AddForce (gameObject.transform.up * speed * input);
+	}
+}
